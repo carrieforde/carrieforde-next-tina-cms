b899f33c095299fa575affc651fe9a18e7d0a128
Carrie Forde <carrie@carrieforde.com>
2024-03-03 11:20:02 -0800

    remove storybook redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => [
-    // Order matters here! The first matching rewrite is used.
-    {
-      source: "/components",
-      destination: "/storybook/index.html",
-    },
-    {
-      source: "/iframe.html",
-      destination: "/storybook/iframe.html",
-    },
-    {
-      source: "/:path*",
-      destination: "/storybook/:path*",
-    },
-  ],
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
